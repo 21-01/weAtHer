64fbbb46f86d0a67a1a7a0802f0efd259aabb81a
21-01 <banyaklontelontemaudijual+git@gmail.com>
2023-12-25 09:41:25 +0700

    add support for fahrenheit + a bit refactor

--- src/index.js
+++ src/index.js
@@ -10,22 +10,57 @@ const temperature = document.querySelector("#temperature")
 const feelsLike = document.querySelector("#feelsLike")
 const lastUpdate = document.querySelector("#lastUpdate")
 
-async function getData(location){
-    const response = await fetch(`#http://api.weatherapi.com/v1/current.json?key=8c16a0f9726049ebb9113634232512&q=${location}`,{ mode: "cors" })
-    const weatherData = await response.json();
 
-    region.innerHTML = weatherData.location.name
-    conditionLogo.src = weatherData.current.condition.icon
-    condition.innerHTML = weatherData.current.condition.text
-    country.innerHTML = weatherData.location.country
-    temperature.innerHTML = `${weatherData.current.temp_c}℃`
-    feelsLike.innerHTML = `Feels like ${weatherData.current.feelslike_c}℃`
-    lastUpdate.innerHTML = weatherData.current.last_updated
+let unit = "c"
+let weatherJson
+
+
+async function getData(location){
+    try{
+        const response = await fetch(`http://api.weatherapi.com/v1/current.json?key=8c16a0f9726049ebb9113634232512&q=${location}`,{ mode: "cors" })
+        const weatherData = await response.json();
+        weatherJson = weatherData
+        console.log(weatherJson)
+    }catch(err){
+        console.error(err);
+    }
 }
 
-getData("jakarta")
+function weatherDom(){
+    region.innerHTML = weatherJson.location.name
+    conditionLogo.src = weatherJson.current.condition.icon
+    condition.innerHTML = weatherJson.current.condition.text
+    country.innerHTML = weatherJson.location.country
+    if (unit=="c"){
+        temperature.innerHTML = `${weatherJson.current.temp_c}℃`
+        feelsLike.innerHTML = `Feels like ${weatherJson.current.feelslike_c}℃`
+    }else{
+        temperature.innerHTML = `${weatherJson.current.temp_f}℉`
+        feelsLike.innerHTML = `Feels like ${weatherJson.current.feelslike_f}℉`
+    }
+    lastUpdate.innerHTML = weatherJson.current.last_updated
+}
+
+async function getWeather(location){
+    await getData(location)
+    await weatherDom()
+}
+
+
+getWeather("jakarta")
+
 
 searchBtn.addEventListener("click", ()=>{
-    getData(searchBar.value)
+    getWeather(searchBar.value)
+})
+
+temperature.addEventListener("click",()=>{
+    if (unit=="c"){
+        unit = "f"
+    }else{
+        unit = "c"
+    }
+    console.log(unit)
+    weatherDom()
 })
 
